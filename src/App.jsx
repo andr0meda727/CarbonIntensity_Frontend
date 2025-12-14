8ecd0ed7d55e4b743b1fdf50ed2807833cf71374
kwwk <kwwk747@gmail.com>
2025-12-14 19:53:26 +0100

    Add main app layout with energy mix forecast and charging window
    I integrated Header, EnergyMixCard, and OptimalChargingWindow components. I handle loading and error states.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import './App.css'
 import Header from "./components/Header.tsx";
+import EnergyMixCard from "./components/EnergyMixCard.tsx";
+import useFetchEnergyMix from "./hooks/useFetchEnergyMix.tsx";
+import LoadingSpinner from "./components/LoadingSpinner.tsx";
+import OptimalChargingWindow from "./components/OptimalChargingWindow.tsx";
 
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  )
+    const {data, isLoading, isError} = useFetchEnergyMix();
+
+    if (isLoading) return <LoadingSpinner />;
+    if (isError) return <h1>Error</h1>;
+
+    return (
+        <>
+            <Header/>
+
+            <div className="max-w-5xl mx-auto">
+                <div className="flex flex-col gap-1 mt-8">
+                    <h2 className={"text-4xl text-white text-center font-bold"}>Energy mix forecast</h2>
+                    <h3 className={"text-gray-400 text-center tracking-widest"}>Reduce your carbon footprint.</h3>
+                </div>
+
+                <div className="flex flex-col lg:flex-row justify-center items-center gap-8 mt-16">
+                    {data?.energyMixDays.map((day) => (
+                        <EnergyMixCard key={day.date} dayData={day} />
+                    ))}
+                </div>
+
+                <OptimalChargingWindow />
+            </div>
+        </>
+    )
 }
 
 export default App
