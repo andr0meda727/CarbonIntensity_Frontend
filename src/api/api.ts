fa885e6caf8908f8b32a22ed45759634334e72cb
kwwk <kwwk747@gmail.com>
2025-12-14 20:04:38 +0100

    Change baseURL

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import {EnergyMixResponse, OptimalWindowResponse} from '../types/types';
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export const api = axios.create({
-    baseURL: 'https://localhost:44302/api/CarbonIntensity',
+    baseURL: `${BASE_URL}/api/carbonintensity`,
     headers: {
         'Content-Type': 'application/json',
     },
