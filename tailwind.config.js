65b13ad575ff19a22514322def31f6c67984a15b
kwwk <kwwk747@gmail.com>
2025-12-14 12:24:36 +0100

    Add basic project structure and conf

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary-bg': '#10221A',
+                'energy-card-bg': '#1C2E26',
+                'button-bg': '#13EC92',
+                'textMain': '#FFFFFF',
+                'textSecondary': '#9CA3AF',
+            },
+        },
+    },
+    plugins: [],
+}
