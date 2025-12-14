61abe4134001d660577e1f1e81031ac08780c89e
kwwk <kwwk747@gmail.com>
2025-12-14 17:14:14 +0100

    Add EnergyMixCard component
    I had to transform data from api into chartData array, because it is required by Recharts. I filtered data, so energy types with 0 share are not displayed.
    I also added SOURCE_COLORS, so data is nicely displayed in the PieChart.

---
+++ src/components/EnergyMixCard.tsx
@@ -0,0 +1,38 @@
+import EnergyPieChart from "./EnergyPieChart";
+import CleanEnergyDisplay from "./CleanEnergyDisplay.tsx";
+import { energyMixDay } from "../types/types.ts";
+
+const SOURCE_COLORS: Record<string, string> = {
+    biomass: "#8D6E63",
+    coal: "#20262c",
+    imports: "#768e9c",
+    gas: "#FF9800",
+    nuclear: "#0f5902",
+    hydro: "#2196F3",
+    solar: "#FFD700",
+    wind: "#13EC92",
+    other: "#9E9E9E"
+};
+
+const EnergyMixCard = ({ dayData }: { dayData: energyMixDay }) => {
+    const chartData = Object.entries(dayData.averageEnergyMix)
+        .map(([energyType, percentageShare]) => ({
+            name: energyType,
+            value: percentageShare,
+            fill: SOURCE_COLORS[energyType] || "#cccccc" // fallback
+        }))
+        .filter(item => item.value > 0);
+
+
+    return (
+        <div className="bg-energyCard rounded-xl shadow-lg w-80 text-white border-2 border-buttonBg/10 flex flex-col p-4">
+            <h3 className="text-2xl font-bold text-center -mb-4">
+                {new Date(dayData.date).toLocaleDateString()}
+            </h3>
+            <EnergyPieChart data={chartData} />
+            <CleanEnergyDisplay dayData={dayData} />
+        </div>
+    );
+};
+
+export default EnergyMixCard;
