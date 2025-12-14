262b69330e116070057cc4ae1bd89fb20405ab10
kwwk <kwwk747@gmail.com>
2025-12-14 21:14:27 +0100

    Repair LoadingSpinner

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ const LoadingSpinner = () => {
         <div className="flex flex-col justify-center items-center w-full h-80">
             <h1 className={"text-white text-3xl font-bold mb-4"}>It's worth waiting :D</h1>
             <div
-                className="animate-spin rounded-full h-16 w-16 border-4 border-buttonBg"
+                className="animate-spin rounded-full h-16 w-16 border-4 border-buttonBg border-t-transparent"
             ></div>
         </div>
     );
